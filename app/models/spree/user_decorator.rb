3dbd2bdbaa9f080749c539ba535b327b67cfd899
sunny2601 <sunnysarrot@gmail.com>
2015-05-02 22:22:38 +0530

    Update user_decorator.rb

--- app/models/spree/user_decorator.rb
+++ app/models/spree/user_decorator.rb
@@ -1,6 +1,7 @@
 module Spree
   Spree.user_class.class_eval do
     has_many :products
+    has_many :orders
     has_and_belongs_to_many :roles, join_table: 'spree_roles_users', class_name: 'Spree::Role'
   end
 end
