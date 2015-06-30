c133d0eff2268f8e2b9af6ef5775fef86dcf80bc
sunny2601 <sunnysarrot@gmail.com>
2015-06-30 19:57:39 +0530

    Update 20130709105614_add_editable_is_default_and_index_on_editable_is_default_and_name_to_spree_roles.rb

--- db/migrate/20130709105614_add_editable_is_default_and_index_on_editable_is_default_and_name_to_spree_roles.rb
+++ db/migrate/20130709105614_add_editable_is_default_and_index_on_editable_is_default_and_name_to_spree_roles.rb
@@ -2,8 +2,10 @@ class AddEditableIsDefaultAndIndexOnEditableIsDefaultAndNameToSpreeRoles < Activ
   def change
     add_column :spree_roles, :editable, :boolean, :default => true
     add_column :spree_roles, :is_default, :boolean, :default => false
+    add_column :spree_roles, :discount_percentage, :float, :default => 0
 
     add_index(:spree_roles, :name)
+    add_index(:spree_roles, :discount_percentage)
     add_index(:spree_roles, :is_default)
     add_index(:spree_roles, :editable)
   end
